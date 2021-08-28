af9580757fc5647354069854828c572463538829
gnarium <80353633+gnarium@users.noreply.github.com>
2021-08-28 13:38:49 +0530

    oden

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,6 @@ import Head from 'next/head'
 import Link from 'next/link'
 import React, { useRef, useState , Component } from "react";
 import ReactHover from 'react-hover'
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
-
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
